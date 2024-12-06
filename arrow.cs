b3a564d0229c506e7b95db64227b412dae085da8
Jch4ipas <jeremy.wuersch@gmail.com>
2024-12-06 17:21:00 +0100

    [update] change while to for and put error

--- arrow.cs
+++ arrow.cs
@@ -1,12 +1,27 @@
-Console.Write("Quelle est la taille de ta flêche (nombre entre 0 et 57) ? ");
+Console.Write("Veuillez entrez un nombre entier entre 2 et 20 : ");
 var sizestring = Console.ReadLine();
+if (!int.TryParse(sizestring, out _))
+{
+    Console.WriteLine("Votre nombre doit être un entier.");
+    return;
+}
 int sizeint;
 sizeint = Convert.ToInt32(sizestring);
+if (sizeint < 2){
+    Console.Write("Erreur, Veuillez écrire un nombre entre 2 et 20.");
+    Console.Write("\n");
+    return;
+}
+if (sizeint > 20){
+    Console.Write("Erreur, Veuillez écrire un nombre entre 2 et 20.");
+    Console.Write("\n");
+    return;
+}
+
 int line = 1;
 int gap = 1;
 int espace;
 espace = sizeint - 1;
-int chang = 1;
 int queufleche = sizeint - 1;
 
 if (sizeint == 0){
@@ -18,27 +33,26 @@ if (sizeint == 1){
     return;
 }
 for (int i = 0; i < espace + 1; i++){
-    Console.Write(" ");
+        Console.Write(" ");
 }
 Console.Write("*");
 Console.Write("\n");
-while (chang == 1){
-    while (line != sizeint - 1){
-        for (int i = 0; i < espace; i++){
+
+for (int n = 0; n < sizeint - 2; n++){
+    for (int i = 0; i < espace; i++){
             Console.Write(" ");
         }
-        espace--;
-        Console.Write("*");
-        for (int i = 0; i < gap; i++){
-            Console.Write(" ");
-        }
-        Console.Write("*");
-        gap = gap + 2;
-        line++;
-        Console.Write("\n");
+    espace--;
+    Console.Write("*");
+    for (int i = 0; i < gap; i++){
+        Console.Write(" ");
     }
-        chang = 2;
+    Console.Write("*");
+    gap = gap + 2;
+    line++;
+    Console.Write("\n");
 }
+
 Console.Write(" ");
 for (int i = 0; i < sizeint * 2 - 1; i++){
     Console.Write("*");
